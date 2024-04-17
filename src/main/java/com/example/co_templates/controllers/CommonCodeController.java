8a8725bfa4056d5515653b8e128916ea1e489b58
youjinkim <kelly2.youjin.kim@gmail.com>
2024-04-17 06:03:17 +0000

    commit

--- src/main/java/com/example/co_templates/controllers/CommonCodeController.java
+++ src/main/java/com/example/co_templates/controllers/CommonCodeController.java
@@ -1,10 +1,8 @@
 package com.example.co_templates.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,22 +18,22 @@ public class CommonCodeController {
     @Autowired
     CommonCodeService commonCodeService;
 
-    // @GetMapping("/commonCode/list")
-    public ModelAndView list(ModelAndView modelAndView
-                        ,@RequestParam HashMap<String, Object> dataMap
-                        ,@RequestParam(name = "deleteIds", required = false ) ArrayList<String> deleteIds) {
-        ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
-        // Call Service with Pure Java
-        // CommonCodeService commonCodeService = new CommonCodeService();
-        itemList = (ArrayList<HashMap<String, Object>>) commonCodeService.selectMany(dataMap);
+    // // @GetMapping("/commonCode/list")
+    // public ModelAndView list(ModelAndView modelAndView
+    //                     ,@RequestParam HashMap<String, Object> dataMap
+    //                     ,@RequestParam(name = "deleteIds", required = false ) ArrayList<String> deleteIds) {
+    //     ArrayList<HashMap<String, Object>> itemList = new ArrayList<HashMap<String, Object>>();
+    //     // Call Service with Pure Java
+    //     // CommonCodeService commonCodeService = new CommonCodeService();
+    //     itemList = (ArrayList<HashMap<String, Object>>) commonCodeService.selectMany(dataMap);
 
-        String viewPath = "/WEB-INF/views/commoncode/list.jsp";
-        modelAndView.setViewName(viewPath);
-        modelAndView.addObject("itemList", itemList);
-        modelAndView.addObject("dataMap", dataMap);
+    //     String viewPath = "/WEB-INF/views/commoncode/list.jsp";
+    //     modelAndView.setViewName(viewPath);
+    //     modelAndView.addObject("itemList", itemList);
+    //     modelAndView.addObject("dataMap", dataMap);
 
-        return modelAndView;
-    }    
+    //     return modelAndView;
+    // }    
 
     @GetMapping("/commonCode/list")
     public ModelAndView listWithDB(ModelAndView modelAndView
